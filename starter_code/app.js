76792210b7873af4dcf14e62e931589e4e882f00
olgabon <olga.bondarenko.shams@gmail.com>
2019-05-01 23:14:38 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,15 +7,41 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers, title: "Beer List"});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    var beer = beers[0];
+    res.render('randomBeer', {beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 });
 
 
 
-app.listen(3000);
+app.listen(3001);
